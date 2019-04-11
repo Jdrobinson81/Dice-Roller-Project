e254f4b1d19449f2847b0b968ccc51830cd65e28
Jimmy Robinson <jimdrobinson81@gmail.com>
2019-04-10 17:01:13 -0700

    Add files via upload

---
+++ Dice Roller Visual/Dice Roller Visual/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Dice_Roller_Visual
+{
+    public partial class Connection : Component
+    {
+        public Connection()
+        {
+            InitializeComponent();
+            Socket s = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
+
+
+
+        }
+
+        public Connection(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
